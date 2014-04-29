cf60f6ccc8db0c33178d24779503dca151d53026
Sebi Kovacs <sebi.kovacs@gmail.com>
2014-04-29 11:30:57 +0300

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+/*
+ * gruntmysql
+ * http://sebastiankovacs.com/gruntplugin
+ *
+ * Copyright (c) 2014 Sebi Kovacs
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>',
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      },
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp'],
+    },
+
+    // Configuration to be run (and then tested).
+    mysql: {
+      custom_options: {
+        options: {
+          host: 'localhost',
+          port: '3306',
+          user: 'root',
+          password: 'admin',
+          database: 'adlogica',
+          table: 'Design',
+          field: 'design_html'
+        },
+        files: {
+          'tmp/custom_options': ['test/fixtures/index.html'], 
+        },
+      },
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js'],
+    },
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'mysql', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
