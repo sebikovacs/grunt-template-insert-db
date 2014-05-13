8149fe315ec7871655fbbd21c77edd5b896f7af6
Sebi Kovacs <sebi.kovacs@gmail.com>
2014-05-13 12:07:05 +0300

    Updated SQL

--- tasks/insertTemplate.js
+++ tasks/insertTemplate.js
@@ -9,34 +9,37 @@
 'use strict';
 
 var mysql      = require('mysql');
-var fs = require("fs");
+var async      = require('async');
+var fs         = require("fs");
 
 module.exports = function(grunt) {
   grunt.registerMultiTask('insertTemplate', 'Insert templates into MySQL database', function() {
-      var self = this;
+      
       var options = this.options();
       var connection = mysql.createConnection(options);
+      var done = this.async();
 
       this.files.forEach(function (file) {
-        file.src.filter(function (filepath) {
-          var done = self.async();
-
-          if (!grunt.file.exists(filepath)) {
-            done(false);
-          } else {
-            var filecontent = grunt.file.read(filepath);
-            var sql = "INSERT INTO " + options.table + " (" + options.field + ") VALUES ('"+ filecontent +"')";
-
-            connection.query(sql, function (err, result) {
-              if(err) {
-                done(false);
-              }
-              if (result.affectedRows == 1) {
-                grunt.log.writeln('\nDatabase updated successfully. Rows affected: ' + result.affectedRows +'. ' + 'Insert ID: ' + result.insertId);
-              }
-              done();
-            });
+        var sql = "INSERT INTO " + options.table + " (" + options.field + ") VALUES ";
+        
+        for(var i = file.src.length - 1; i >= 0; i--) {
+          var filecontent = grunt.file.read(file.src[i]);
+          var values = "('"+ filecontent +"')";
+          sql += values;
+          if (i > 0) {
+            sql += ',';
           }
+        }
+
+        connection.query(sql, function (err, result) {
+          if(err) {
+            done(false);
+          }
+          if (result && result.affectedRows === 1) {
+
+            grunt.log.writeln('\nDatabase updated successfully.');
+          }
+          done();
         });
       });
 
